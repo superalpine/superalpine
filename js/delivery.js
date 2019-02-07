1d7ea443e33458f2a7f1ee9b5d1dfc79876961bf
Andrew <hello@superalpine.co>
2019-02-07 15:20:15 +0000

    Countries

--- js/delivery.js
+++ js/delivery.js
@@ -13,38 +13,11 @@ $(document).ready(function(){
                 $(".payment-button").not(".USA").hide();
                 $(".USA").show();
             }
-             else if($(this).attr("value")=="Canada"){
-                $(".payment-button").not(".Canada").hide();
-                $(".Canada").show();
+             else if($(this).attr("value")=="ROW"){
+                $(".payment-button").not(".ROW").hide();
+                $(".ROW").show();
             }
-            else if($(this).attr("value")=="Australia"){
-                $(".payment-button").not(".Australia").hide();
-                $(".Australia").show();
-            }
-             else if($(this).attr("value")=="NewZealand"){
-                $(".payment-button").not(".NewZealand").hide();
-                $(".NewZealand").show();
-            }
-             else if($(this).attr("value")=="Japan"){
-                $(".payment-button").not(".Japan").hide();
-                $(".Japan").show();
-            }
-             else if($(this).attr("value")=="Swiss"){
-                $(".payment-button").not(".Swiss").hide();
-                $(".Swiss").show();
-            }
-             else if($(this).attr("value")=="Other"){
-                $(".payment-button").not(".Other").hide();
-                $(".Other").show();
-            }
-             else if($(this).attr("value")=="UAE"){
-                $(".payment-button").not(".UAE").hide();
-                $(".UAE").show();
-            }
-             else if($(this).attr("value")=="SaudiArabia"){
-                $(".payment-button").not(".SaudiArabia").hide();
-                $(".SaudiArabia").show();
-            }
+            
             else{
                 $(".payment-button").hide();
             }
